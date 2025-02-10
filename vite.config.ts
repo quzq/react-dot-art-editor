d2ea6545fc773b9255e39c79f1544b5980eb6b9c
quzq <user@MSI>
2025-02-11 03:33:48 +0900

    revice config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-dot-art-editor/", // リポジトリ名を指定
   plugins: [react()],
-})
+});
